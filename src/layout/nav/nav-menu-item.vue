872e71d8498b69cf5dbbe2aba253bf83e8391b0f
lixin <lx000166@outlook.com>
2022-07-12 18:32:59 +0800

    锁定依赖版本 bug修复

--- src/layout/nav/nav-menu-item.vue
+++ src/layout/nav/nav-menu-item.vue
@@ -1,16 +1,16 @@
 <!--
  * @Author: lx000
  * @Date: 2021-11-14 20:01:25
- * @LastEditTime: 2021-11-19 00:14:40
+ * @LastEditTime: 2022-07-12 18:00:30
  * @Description: 描述
 -->
 <script lang="ts" setup>
-  defineProps<{
-    menuitem: menuObj[];
-  }>();
+defineProps<{
+  menuitem: menuObj[];
+}>();
 </script>
 <template>
-  <template v-for="menu: menuObj in menuitem">
+  <template v-for="menu in menuitem">
     <a-sub-menu v-if="menu.type == 'submenu'" :key="menu.id">
       <template #title>
         <span>{{ menu.lable }}</span>
@@ -22,8 +22,12 @@
   </template>
 </template>
 <style scoped>
-  .a-sub-menu-title {
-    font-size: 0.75rem /* 12px */;
-    margin-left: 0.5rem /* 8px */;
-  }
+.a-sub-menu-title {
+  font-size: 0.75rem
+    /* 12px */
+  ;
+  margin-left: 0.5rem
+    /* 8px */
+  ;
+}
 </style>
